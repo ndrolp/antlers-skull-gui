ea89dee417ae6c1dfb5d0da33d975ebc2176cc09
Ndrolp <ndro.lozano@gmail.com>
2024-08-23 19:32:10 +0200

    chore: create isElectron hook

--- src/core/hooks/useGetConfig.tsx
+++ src/core/hooks/useGetConfig.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react'
+import useIsElectron from './useIsElectron'
 
 export interface ConfigType {
     url: string
 }
 
 export default function useGetConfig(iterator = 0) {
-    const [config, setConfig] = useState<ConfigType | null>(null)
+    const [config, setConfig] = useState < ConfigType | null > (null)
+    const isElectron = useIsElectron()
 
     useEffect(() => {
-        window.ipcRenderer.invoke('get-config', 'ping').then(result => {
-            setConfig(result)
-        })
+        if (isElectron) {
+            window.ipcRenderer.invoke('get-config', 'ping').then(result => {
+                setConfig(result)
+            })
+        }
     }, [iterator])
 
     return config
--- src/features/users/pages/Users.tsx
+++ src/features/users/pages/Users.tsx
@@ -1,8 +1,15 @@
+import { Button } from '@/components/ui/button'
 import { UsersTable } from '../components/UsersTable'
+import { UserRoundPlus } from 'lucide-react'
 
 export const Users = () => {
     return (
-        <div>
+        <div className='flex flex-col gap-2'>
+            <div>
+                <Button variant='outline'>
+                    <UserRoundPlus className='mr-2 h-4 w-4' /> Create
+                </Button>
+            </div>
             <UsersTable />
         </div>
     )

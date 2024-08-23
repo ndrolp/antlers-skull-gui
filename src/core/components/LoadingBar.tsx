fc177e4f6ee5d50f081489fa1d33bb04a67c83ab
Ndrolp <ndro.lozano@gmail.com>
2024-08-23 18:31:12 +0200

    feat: create a loading bar

---
+++ src/core/components/LoadingBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/loadingBar.css'
+
+export default function LoadingBar() {
+    return (
+        <div className='loader'>
+            <div className='loaderBar bg-primary'></div>
+        </div>
+    )
+}
--- src/features/users/components/UsersTable.tsx
+++ src/features/users/components/UsersTable.tsx
@@ -2,14 +2,15 @@ import { DataTable } from '@/core/components/DataTable'
 import usersColumns from '../dataModels'
 import useGetUsers from '../hooks/useGetUsers'
 import { useState } from 'react'
+import LoadingBar from '@/core/components/LoadingBar'
 
 export const UsersTable = () => {
     const [page] = useState(1)
     const { users, loading } = useGetUsers({ page })
     return (
         <div>
-            {loading ? <h1>Loading</h1> : ''}
             <DataTable data={users[page] ?? []} columns={usersColumns} />
+            {loading ? <LoadingBar /> : ''}
         </div>
     )
 }
